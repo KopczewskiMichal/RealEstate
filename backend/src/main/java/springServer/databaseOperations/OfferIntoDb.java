578cc48085d295d36b78d812f24f7338cc2aa9f3
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-06-03 23:53:52 +0200

    Jeśli użytkownik jest w bazie ale ze starymi danymi to go aktualizuje

--- backend/src/main/java/springServer/databaseOperations/OfferIntoDb.java
+++ backend/src/main/java/springServer/databaseOperations/OfferIntoDb.java
@@ -12,7 +12,7 @@ import com.mongodb.client.model.Filters;
 
 // * Z założenia obiekt tej klasy jest krótkotrwały, mamy nadzieję że baza nie padnie w trakciejego istnienia
 final class OfferIntoDb {
-    private final String mongoUri; // * Nie da się pobrać za pomocą @Value spoza komponentu Springa
+    // * Nie da się pobrać mongouri za pomocą @Value spoza komponentu Springa
 
     private User user;
 
@@ -20,20 +20,14 @@ final class OfferIntoDb {
     private final MongoDatabase database;
 
     OfferIntoDb(String mongoUri) {
-        this.mongoUri = mongoUri;
-        System.out.println("Zaraz tworzymy połączenie do bazy danych." + mongoUri);
         mongoClient = MongoClients.create(mongoUri);
-        System.out.println("Sieeema, zaraz łączymy sie z bazą w bazie xd");
         database = mongoClient.getDatabase("RealEstate");
-        System.out.println("to mongouri" + mongoUri);
-        System.out.println("database" + database);
     }
 
     void insertOfferIntoDb (Place place, User user) throws CustomDatabaseException{
-        System.out.println(this.mongoUri);
         this.user = user;
         try {
-            registerUserIfNeeded();
+            registerOrUpdateUserIfNeeded();
             MongoCollection<Document> offersCollection = database.getCollection("Offers");
             Document newOffer = Document.parse(place.toJson().toString());
             offersCollection.insertOne(newOffer);
@@ -45,13 +39,16 @@ final class OfferIntoDb {
         }
     }
 
-    private void registerUserIfNeeded () {
-        System.out.println("Jestem w register ...");
+    private void registerOrUpdateUserIfNeeded() {
         MongoCollection<Document> usersCollection = database.getCollection("Users");
-        Document user = usersCollection.find(Filters.eq("email", this.user.getEmail())).first();
-        if (user == null) {
-            Document newUser = Document.parse(this.user.toJson().toString());
+        Document existingUser = usersCollection.find(Filters.eq("email", this.user.getEmail())).first();
+        Document newUser = Document.parse(this.user.toJson().toString());
+        if (existingUser == null) {
             usersCollection.insertOne(newUser);
+        } else {
+            if (!existingUser.equals(newUser)) {
+                usersCollection.replaceOne(Filters.eq("email", this.user.getEmail()), newUser);
+            }
         }
     }
 }
