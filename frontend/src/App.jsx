d5429abc68090d75c299ee509569efa90e47db03
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-06-16 13:42:08 +0200

    Sprawne zmienne środowiskowe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import MainPage from './components/mainPage/MainPage.jsx';
 
 function App() {
--- frontend/src/components/mainPage/MainPage.jsx
+++ frontend/src/components/mainPage/MainPage.jsx
@@ -1,7 +1,29 @@
 import logo from './../../logo.svg';
 import './../../App.css';
 
+import axios from "axios";
+import React, { useEffect, useState} from "react";
+
+
+
 export default function MainPage () {
+  const [offers, setOffers] = useState([]);
+
+  const getOffers = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/all-offers`).then((res) => {
+      setOffers(res.data);
+      console.log(offers);
+    }).catch((err) => {
+      console.error(err.getMessage);
+    }).finally(() => {
+      console.log("Wykonano próbę pobrania danych z api")
+    });
+  };
+
+  useEffect(() => {
+    getOffers();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +31,7 @@ export default function MainPage () {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>URL api: {process.env.REACT_APP_API_URL}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
