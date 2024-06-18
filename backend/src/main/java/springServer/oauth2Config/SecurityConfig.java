9bbd0e833c6e09deb193891fa8b163ae35315c5b
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-06-18 17:19:14 +0200

    PO cofnięciu zmian na backendzie

--- backend/src/main/java/springServer/oauth2Config/SecurityConfig.java
+++ backend/src/main/java/springServer/oauth2Config/SecurityConfig.java
@@ -1,58 +1,30 @@
 package springServer.oauth2Config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${frontend.url}")
-    private String frontendUrl;
-
-    @Value("${jwt.secret_key}")
-    private String secretKey;
-
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        JWTAuthorizationFilter jwtAuthorizationFilter = new JWTAuthorizationFilter(secretKey);
-
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/").permitAll();
-
                     auth.requestMatchers("/all-offers").permitAll();
 //                    auth.requestMatchers("/admin").hasRole("ADMIN").anyRequest().authenticated();
                     auth.anyRequest().authenticated();
                 })
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/loginSuccess", true))
-                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendUrl));
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
-        return urlBasedCorsConfigurationSource;
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .build();
     }
 }
