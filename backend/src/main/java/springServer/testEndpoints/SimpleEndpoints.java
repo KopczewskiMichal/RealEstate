be57d0d009c661680a42ced49fa8577562bac3d4
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-06-23 12:38:02 +0200

    Usunięto poprzednią wersję całego frontendu

--- backend/src/main/java/springServer/testEndpoints/SimpleEndpoints.java
+++ backend/src/main/java/springServer/testEndpoints/SimpleEndpoints.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@Deprecated
 @RestController
 public class SimpleEndpoints {
 
