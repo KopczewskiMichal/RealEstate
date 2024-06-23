96d8cadbb7622d8614956cf520223f8a591e44d4
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-06-23 18:37:33 +0200

    Budowanie api do obrazu docker

--- backend/src/main/java/springServer/oauth2Config/SecurityConfig.java
+++ backend/src/main/java/springServer/oauth2Config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package springServer.oauth2Config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
